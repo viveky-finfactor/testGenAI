a75726072327fc43fd9a3bc5016e83c2bb7e1635
viveky <viveky@finfactor.in>
2025-04-17 11:40:41 +0530

    initial commit

---
+++ src/test/java/com/ftpl/testGenAI/TestGenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftpl.testGenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
